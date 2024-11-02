036a28bff03263b2848f0d2b9620afc14356496f
Jesús Eduardo Reyes Ramos <jesus_99reyes@hotmail.com>
2024-11-02 06:53:42 -0500

    Ocultar "premios" del admin

--- login/torneos/admin.py
+++ login/torneos/admin.py
@@ -5,7 +5,8 @@ class InscripcionTorneoInline(admin.TabularInline):
     model = InscripcionTorneo
     extra = 0
     readonly_fields = ('premio_calculado',)  # Mostrar el cálculo en lugar del premio guardado
-
+    exclude = ('premio',)
+    
     def premio_calculado(self, instance):
         return instance.premio_calculado  # Uso directo de la propiedad para consistencia
 
